b8350172c18c95e1c002de742fddb7fe203f9231
Divyanshi-03 <85453778+Divyanshi-03@users.noreply.github.com>
2021-08-11 16:34:01 +0530

    Create combination.cpp

---
+++ recursion/combination.cpp
@@ -0,0 +1,46 @@
+// method 1 
+#include <stdio.h>
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+double factorial(double n)
+{
+    if(n==0)
+    return 1;
+    
+    else return n*factorial(n-1);
+    
+}
+
+int main()
+{
+    double n,r;
+    cin>>n>>r;
+    cout<<"nCr="<< factorial(n)/(factorial(r)*factorial(n-r));
+
+    return 0;
+}
+// method 2 
+#include <stdio.h>
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+double nCr(int n, int r)
+{
+    if(r==0|| r==n)
+    return 1;
+    else 
+    return nCr(n-1,r)+nCr(n-1,r-1);
+    
+}
+
+int main()
+{
+    double n,r;
+    cin>>n>>r;
+    cout<<"nCr="<<nCr(n,r) ;
+
+    return 0;
+}
