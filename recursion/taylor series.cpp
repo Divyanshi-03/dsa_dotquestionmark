e93a26c95edec51f75559e37b7f1c32a8137b7ad
Divyanshi-03 <85453778+Divyanshi-03@users.noreply.github.com>
2021-08-16 15:10:56 +0530

    Update taylor series.cpp

--- recursion/taylor series.cpp
+++ recursion/taylor series.cpp
@@ -30,3 +30,42 @@ int main()
 
     return 0;
 }
+***********************************************************************************************************************************************************
+// better method 
+double taylorseries(double x, double n)
+{
+    if(n==0)
+    return 1; 
+    else
+    static int pow=1,fac=1;
+    double term=taylorseries(x,n-1)
+    pow=pow*x;
+    fac=fac*n
+   
+    return (term + pow/fac) ;
+}
+// loop for this (withoit recursion method
+double taylorloop(int x, int n)
+{
+    double series=1;
+    double num =1, den=1;
+    for( int i=0;i<n;i++)
+    {
+        num=num*x;
+        den*=i;
+        series+=num/den;}
+    return series; }
+************************************************************************************************************************************************************
+// method 3 
+// using homers rule 
+double taylor(double x, double n)
+{
+    static double temp =1;
+    if(n==0)
+        return temp;
+    else{
+        temp = 1+ (x/n*temp);
+        return taylor(x,n-1);}
+}
+    
+
