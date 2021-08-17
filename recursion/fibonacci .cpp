8a065561b3f97658b8bcc92123599b07541ea0d9
Divyanshi-03 <85453778+Divyanshi-03@users.noreply.github.com>
2021-08-17 16:41:28 +0530

    Update fibonacci .cpp

--- recursion/fibonacci .cpp
+++ recursion/fibonacci .cpp
@@ -21,3 +21,25 @@ int main()
 
     return 0;
 }
+*********************************************************************************************************************************************************************
+//using memorization 
+int memory[1000]
+int fibonacci(int n)
+{
+    if(n<=1)
+    {  memory[n]=n;
+    return n;}
+    else
+    {
+        if(memory[n-1]==-1)
+        {
+            memory[n-1]= fibonacci(n-1);
+            }
+        if(memory[n-2]==-1)
+        { 
+            memory[n-2]= fibonacci(n-2);
+            }
+        memory[n]=memory[n-1]+memory[n-2];
+        return memory[n];
+    }
+}
